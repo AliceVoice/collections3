b7f046b363717e8740387a4510295dd7f601a1ec
AliceVoice <poctuk22@gmail.com>
2024-02-12 00:24:42 +0300

    Initial commit

---
+++ src/main/java/collectionsAndDiversity/collectionsAndDiversity/CollectionsAndDiversityApplication.java
@@ -0,0 +1,13 @@
+package collectionsAndDiversity.collectionsAndDiversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsAndDiversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsAndDiversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/collectionsAndDiversity/collectionsAndDiversity/CollectionsAndDiversityApplicationTests.java
@@ -0,0 +1,13 @@
+package collectionsAndDiversity.collectionsAndDiversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsAndDiversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
